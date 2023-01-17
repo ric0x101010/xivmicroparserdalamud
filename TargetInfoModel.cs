176be87b845966acd332f98323987c691e0e0783
Rico <elbdom@gmail.com>
2023-01-17 14:51:35 +0100

    Update TargetInfoModel.cs

--- TargetInfoModel.cs
+++ TargetInfoModel.cs
@@ -1,1 +1,9 @@
-
+namespace XIVMicroPaserDalamud.Models
+{
+    public class TargetInfoModel
+    {
+        public float HpRatio { get; set; }
+        public double Rdps { get; set; }
+        public string RemainingTime { get; set; }
+    }
+}
