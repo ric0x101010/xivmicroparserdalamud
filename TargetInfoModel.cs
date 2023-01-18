7f1c6edab562a80bee55630d0263255c419d9711
Rico <elbdom@gmail.com>
2023-01-18 11:01:28 +0100

    Delete TargetInfoModel.cs

--- TargetInfoModel.cs
+++
@@ -1,9 +0,0 @@
-namespace XIVMicroPaserDalamud.Models
-{
-    public class TargetInfoModel
-    {
-        public float HpRatio { get; set; }
-        public double Rdps { get; set; }
-        public string RemainingTime { get; set; }
-    }
-}
